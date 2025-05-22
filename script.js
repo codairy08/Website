35c2fe9467ea4be3d4ecd6abe16d8b32f4e6e6b1
CODAIRY <samrat.codairy@gmail.com>
2025-05-22 20:21:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 // Add any interactive JavaScript later here (optional)
+// Toggle navbar on small screens
+document.getElementById('navToggle').addEventListener('click', function () {
+  document.querySelector('.nav-links').classList.toggle('show');
+});
+
+function updateDateTime() {
+  const now = new Date();
+  const formatted = now.toLocaleString();
+  document.getElementById("liveDateTime").textContent = `Current time: ${formatted}`;
+}
+
+// Update every second
+setInterval(updateDateTime, 1000);
+updateDateTime();
+
+
 console.log("Website Loaded!");
